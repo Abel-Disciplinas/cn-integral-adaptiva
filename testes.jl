edbb8fa833715ea28929f60984191dc7372e1488
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2020-12-10 11:31:34 -0300

    Corrige testes

--- testes.jl
+++ testes.jl
@@ -1,10 +1,12 @@
-using Test
+using Distributions, Test
 
 include("main.jl")
 include("estrutura.jl")
 
 function tests()
   χ(x,v) = x == 0 ? 0.0 : v
+  σ = 1 / √(2π)
+  Φ(x) = cdf(Normal(0, σ), x)
   funset = [
     (exp,                      0.0,  1.0,          exp(1) - 1, "exp"),
     (x -> x^5,                -0.9,  1.1, (1.1^6 - 0.9^6) / 6, "x⁶"),
@@ -13,7 +15,7 @@ function tests()
     (x -> cos(x),              0.0,   2π,                 0.0, "cos x from 0..2π"),
     (x -> cos(x),              0.0,  π/2,                 1.0, "cos x from 0..π/2"),
     (x -> χ(x, x * log(x)),    0.0,  1.0,               -0.25, "x * log(x)"),
-    (x -> exp(-x^2),          -6.0,  6.0,             sqrt(π), "e⁻ˣ²"),
+    (x -> exp(-x^2 / 2σ^2),   -1.0,  1.0,    Φ(1.0) - Φ(-1.0), "N(0,σ²)"),
     (x -> x < π ? 0.0 : 1.0,   3.0,  4.0,               4 - π, "irrational step"),
     (x -> 1 / (1 + exp(-5x)), -1.0,  1.0,                 1.0, "sigmoid")
   ]
@@ -21,10 +23,12 @@ function tests()
   @testset "Testes simples" begin
     for (f, a, b, sol, nome) in funset
       F = FuncaoComCache(f)
-      for ϵ in [1e-3, 1e-4, 1e-5, 1e-6],
-        S = simpson_adaptivo(F, a, b)
-        @test abs(S - sol) < ϵ
-        @test 0 < length(F) ≤ 1000
+      @testset "$nome" begin
+        for ϵ in [1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10]
+          S = simpson_adaptivo(F, a, b, ϵ=ϵ)
+          @test abs(S - sol) < 10ϵ
+          @test 0 < length(F) ≤ 600 * √-log10(ϵ)
+        end
       end
     end
   end
